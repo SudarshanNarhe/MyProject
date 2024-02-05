151bbad7a2663bcc75c4e8e81057a967106ae5c9
SudarshanNarhe <sudarshannarhe@gmail.com>
2024-02-05 15:37:18 +0530

    New Changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AboutUsComponent } from '../about-us/about-us.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AboutUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
