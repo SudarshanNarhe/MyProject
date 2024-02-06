25707003e149664fe3db1979896475b3e5df6531
SudarshanNarhe <sudarshannarhe@gmail.com>
2024-02-06 18:57:08 +0530

    One Way Data Binding Changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,4 +10,33 @@ import { AboutUsComponent } from '../about-us/about-us.component';
 })
 export class HomeComponent {
 
+    cityname:string="Pune";
+
+    num:number=45;
+
+    getData(){
+      return 'this is interpolation'
+    }
+
+    path:string='./assets/tree-736885_1280.jpg';
+
+    name:string='Sudarshan'
+
+    isdisable:boolean=true
+
+    msg :string="Hi";
+
+    onMyClick(){
+
+      //console.log("This is Event")
+
+     this.msg=' '+'Hi This is Event'
+
+     
+    }
+
+    // buttonReturn(){
+    //   return 'This is return On button'
+    // }
+
 }
